dff04bb9952204c661a5a51cc5d934e1c219b5db
utkarshtri03 <utkarshtri03@gmail.com>
2023-09-02 19:38:14 +0530

    components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Hero from "./pages/hero";
+import Payments from "./pages/payments";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,7 @@ function App() {
     <>
       <div className="snap-y snap-mandatory h-[100vh] overflow-y-scroll m-0 smlap:hidden">
         <Hero />
+        <Payments />
         {/* <Payments/>
         <Banking/>
         <Boost/>
